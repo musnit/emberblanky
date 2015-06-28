c604f5973eab30923ac4f1eab999ccbebae760c3
musnit <musnit@gmail.com>
2015-06-28 22:31:16 +0700

    refactor web-preview to use ember click

--- app/components/blanky-web.js
+++ app/components/blanky-web.js
@@ -7,21 +7,20 @@ export default Ember.Component.extend({
   loopNum: 1,
   loopingIDs: ['UHGPYzstxO','gBqF9PtfBm','w9zCNnEbfC','mt1s3uZ90p','rZ4dWe9BGU'],
   start: function(){
-    var self = this;
-    var clicked = function(event) {
-        var x = event.clientX;
-        if (x < document.getElementById('device-screen').getBoundingClientRect().width/2){
-            window.orientationController.reset();
-        }
-        else {
-            var ids = self.get('loopingIDs');
-            var num = self.get('loopNum');
-            self.get('blanky').clearPage()
-            self.get('blanky').loadPage(ids[num%ids.length]);
-            self.set('loopNum', num + 1);
-        }
-    };
     this.set('blanky', new BlankyApp(false, this.get('pages')));
-    document.getElementById('top-screen').addEventListener('click', clicked, false);
-  }.on('didInsertElement')
+    this.set('width', document.getElementById('device-screen').getBoundingClientRect().width);
+  }.on('didInsertElement'),
+  click: function(event){
+    var x = event.clientX;
+    if (x < this.get('width')/2){
+        window.orientationController.reset();
+    }
+    else {
+        var ids = this.get('loopingIDs');
+        var num = this.get('loopNum');
+        this.get('blanky').clearPage()
+        this.get('blanky').loadPage(ids[num%ids.length]);
+        this.set('loopNum', num + 1);
+    }
+  }
 });
