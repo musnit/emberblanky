08573e9e6d866982292aa097eee3a4607d824fc2
musnit <musnit@gmail.com>
2015-06-29 13:45:55 +0700

    moved styles and changed appbrocfile

--- AppBrocfile.js
+++ AppBrocfile.js
@@ -1,35 +1,43 @@
 var uglifyJavaScript = require('broccoli-uglify-js');
-var compileES6 = require('broccoli-es6-concatenator');
+var ES6Modules = require('broccoli-es6modules');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 var findBowerTrees = require('broccoli-bower');
 var browserify = require('broccoli-browserify');
+var optimizeRequireJs = require('broccoli-requirejs');
 
-var blanky = 'blanky';
-blanky = pickFiles(blanky, {
+var src = 'blanky/src';
+src = pickFiles(src, {
   srcDir: '/',
-  destDir: 'blankyapp' // move under appkit namespace
+  destDir: ''
 });
 
 var famous = browserify('node_modules/famous');
 
-var sourceTrees = [blanky, 'app/blankylib', famous];
+var sourceTrees = [src, 'app/blankylib', famous];
 
 var appAndDependencies = new mergeTrees(sourceTrees, { overwrite: true });
-
-
-var appJs = compileES6(appAndDependencies, {
-  inputFiles: [
-    'blankyapp/**/*.js'
-  ],
-  outputFile: '/assets/app.js'
+var amdFiles = new ES6Modules(appAndDependencies, {
+  format: 'amd'
 });
 
-appJs = uglifyJavaScript(appJs, {
+uglyAmdFiles = uglifyJavaScript(amdFiles, {
 });
 
-var appCss = 'app/styles/app.css';
+rJSFiles = optimizeRequireJs(uglyAmdFiles, {
+  verbose   : true,
+  requirejs : {
+    name : 'app',
+    out  : 'app.js'
+  }
+});
+
+var appCss = pickFiles('app/styles/app', {
+  srcDir: '/',
+  destDir: 'assets' // move under appkit namespace
+});
 var publicFiles = 'public';
-//var app = new mergeTrees[appJs, appCss, publicFiles]
+var publicAppFiles = 'blanky/public';
+var app = new mergeTrees([rJSFiles, appCss, publicFiles, publicAppFiles]);
 
-module.exports = appJs; //mergeTrees([appJs, appCss, publicFiles])
+module.exports = app;
